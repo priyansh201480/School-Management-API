3fd0a178c996b59a4feffdd813e828c7f6e5cbef
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2025-08-20 15:12:52 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,20 +1,3 @@
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: Number(process.env.DB_PORT || 3306),
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
-// export default pool;
-
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -36,4 +19,5 @@ const pool = mysql.createPool({
 })().catch(err => console.error('MySQL error:', err.message));
 
 export default pool;
+
 
